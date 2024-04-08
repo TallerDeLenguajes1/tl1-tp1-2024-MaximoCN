2048e9529836ed9174428b201b8e5bc452d9a29c
“Maximo <maxicampos178@gmail.com>
2024-04-08 18:13:18 -0300

    agrego modificacion de tp1_1.cpp

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int main(){
     printf("HOLA MUNDO");
+    
+    int num= 2;
+    int *punt= &num;
+    printf("CONTENIDO DEL PUNTERO %d\n", *punt);
+    printf("Direccion de memoria alamcenada : %p\n" ,punt);
+    printf("Direccin de memoria de la variable: %p\n", &num);
+    printf("direccion de memoria del puntero: %p\n", &punt);
+    printf("Tamanio de memoria utilizado por la variable: %zu",sizeof(num));
     return 0;
 }
